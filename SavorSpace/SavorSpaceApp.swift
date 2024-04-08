2e297fa4500e5dcfe42f3cac9bd4d8914f556c22
Nathen Wiley <nzw0037@auburn.edu>
2024-04-08 10:12:30 -0700

    Initial Commit

---
+++ SavorSpace/SavorSpaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SavorSpaceApp.swift
+//  SavorSpace
+//
+//  Created by user250623 on 4/8/24.
+//
+
+import SwiftUI
+
+@main
+struct SavorSpaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
